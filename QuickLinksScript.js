1e94723a65fa1c6cb4b20387f57ea0668bd6cf00
Zac Warner <45380112+geminerald@users.noreply.github.com>
2024-10-07 10:48:30 +0100

    Update QuickLinksScript.js with Shelf links

--- QuickLinksScript.js
+++ QuickLinksScript.js
@@ -28,23 +28,23 @@ const WebsiteList = [
 const KnowledgeBaseList = [
   {
     name: ' Customer Vend Request    ',
-    url: 'https://yuno.shelf-eu.com/read/1607973f-e287-4491-beb2-9246113205f3/?component=auto-suggest&source=shelf&view=dashboard',
+    url: 'https://yuno.shelf-eu.com/read/1607973f-e287-4491-beb2-9246113205f3/',
   },
   {
     name: 'Emergency Credit',
-    url: 'https://yuno.shelf-eu.com/read/0f1e971d-1b5f-4916-a75c-79aff315d6fe/?searchEventId=01J9K5PSG61TNSNSQH4Z630QEZ&source=shelf&trigger=title&view=snippet-view',
+    url: 'https://yuno.shelf-eu.com/read/0f1e971d-1b5f-4916-a75c-79aff315d6fe/',
   },
   {
     name: '  Retention Training',
-    url: 'https://yuno.shelf-eu.com/read/f750a7cb-709e-4043-af14-a75b87c2362f/?searchEventId=01J9K5RH4DFW4F4R5XMJ9FHXY6&source=shelf&trigger=title&view=snippet-view',
+    url: 'https://yuno.shelf-eu.com/read/f750a7cb-709e-4043-af14-a75b87c2362f/',
   },
   {
     name: 'PSO Levy Refund',
-    url: 'https://yuno.shelf-eu.com/read/17cac829-8853-4964-8970-88c3a43223b4/?searchEventId=01J9K5TGWD7RX1NYJS77NAJWKR&source=shelf&trigger=title&view=snippet-view',
+    url: 'https://yuno.shelf-eu.com/read/17cac829-8853-4964-8970-88c3a43223b4/',
   },
   {
     name: 'Estimate Annual Bill FAQs',
-    url: 'https://yuno.shelf-eu.com/read/6c00e924-ceee-4166-ae83-77c6bc54ec47/?searchEventId=01J9K5V64DJS56QYD3ASHGBW7V&source=shelf&trigger=title&view=snippet-view',
+    url: 'https://yuno.shelf-eu.com/read/6c00e924-ceee-4166-ae83-77c6bc54ec47/',
   },
 ];
 
